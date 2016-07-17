793f1af3e36695ace9ea468890c24502ab55339f
Biswajit Paria <biswajitsc@gmail.com>
2016-07-17 09:54:53 +0530

    added git author rename code

---
+++ git-author-rewrite.sh
@@ -0,0 +1,27 @@
+
+#!/bin/sh
+
+# Taken from github.com
+#
+# To replace a particular email id and name in the commit
+# history with a new email id and name. Useful for
+# attributing commits to the right person.
+#
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="your-old-email@example.com"
+CORRECT_NAME="Your Correct Name"
+CORRECT_EMAIL="your-correct-email@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
